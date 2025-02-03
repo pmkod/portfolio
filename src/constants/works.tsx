e36ec6486652c9d97088ab98abcac209003b5f27
Kodossou <pierremariekod@gmail.com>
2025-02-03 00:17:32 +0000

    Add latest works

--- src/constants/works.tsx
+++ src/constants/works.tsx
@@ -1,4 +1,25 @@
 export const works = [
+
+  {
+    name: "Kanea",
+    link: "https://kanea.net",
+    mainPictureName: "kanea-discussion-page.png",
+    description:
+      "Kanea is a social network for sharing experiences. Interact with a caring community and spread joy every day! Join us for a positive and fulfilling experience.",
+  },
+  {
+    name: "Afrisehi",
+    link: "https://afrisehi.com",
+    mainPictureName: "afrisehi-home-page.png",
+    description:
+      "Buy and sell trendy clothing with ease. Discover the latest collections, find exclusive deals, and express your style—all in one platform!",
+  },
+  {
+    name: "Instanly crypto",
+    link: "https://crypto.instanly.com",
+    mainPictureName: "instanly-crypto-home-page.png",
+    description: "Trade cryptocurrencies securely with mobile money payments. Buy, sell, and exchange crypto effortlessly on a fast and user-friendly platform! "
+  },
   {
     name: "Djamla",
     link: "https://djamla.com",
@@ -6,18 +27,11 @@ export const works = [
     description:
       "A UI library designed to provide accessible, customizable components for react.js. Inspired by shadcn/ui, it simplifies the development of consistent interfaces while adapting to your preferred framework.",
   },
-  {
-    name: "Kanea",
-    link: "https://kanea.vercel.app",
-    mainPictureName: "kanea-discussion-page.png",
-    description:
-      "Kanea is a social network for sharing experiences. Interact with a caring community and spread joy every day! Join us for a positive and fulfilling experience.",
-  },
-  {
-    name: "Bim bara",
-    mainPictureName: "bim-bara-home-page.png",
-    link: "https://play.google.com/store/apps/details?id=com.bimgroup.bimbara",
-    description:
-      "Bim bara is a revolutionary mobile application for connecting employers and jobseekers! Simplify your recruitment and job search with this intuitive and efficient platform. Find the perfect job match in just one click! Positive social impact guaranteed.",
-  },
+  // {
+  //   name: "Bim bara",
+  //   mainPictureName: "bim-bara-home-page.png",
+  //   link: "https://play.google.com/store/apps/details?id=com.bimgroup.bimbara",
+  //   description:
+  //     "Bim bara is a revolutionary mobile application for connecting employers and jobseekers! Simplify your recruitment and job search with this intuitive and efficient platform. Find the perfect job match in just one click! Positive social impact guaranteed.",
+  // },
 ];
